7c469408593edcf9e656f28bad57d67a0a6edf2f
Osasbaby <blessingstephain2424e@gmail.com>
2022-11-28 22:35:13 +0100

    CU-341503c - Token Generation

---
+++ src/utils/jwt.js
@@ -0,0 +1,29 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const secretKey = process.env.JWT_KEY;
+/**
+ *
+ */
+export default class jwtHelper {
+  /**
+   * @param {object} payload - The details to be signed
+   * @param {string} secret - The JWT secret key
+   * @returns {string} The JWT signed token
+   */
+  static async generateToken(payload, secret = secretKey) {
+    const token = await jwt.sign(payload, secret, { expiresIn: '1h' });
+    return token;
+  }
+
+  /**
+   * @param {object} payload - The details to be signed
+   * @param {string} secret - The JWT secret key
+   * @returns {string} The JWT signed token
+   */
+  static async accessToken(payload, secret = secretKey) {
+    const token = await jwt.sign(payload, secret, { expiresIn: '10d' });
+    return token;
+  }
+}
