90cef7ec0d13d5a1b4780a2d56ccadfdd0abb17a
kelue <austincmoon5@gmail.com>
2022-11-28 15:14:06 +0100

    add requested changes from review

--- database/database.js
+++ database/database.js
@@ -1,14 +1,12 @@
 require('dotenv').config();
 
-const { DB_USER, DB_PASSWORD, DB, DB_HOST } = process.env;
-// import env variables for test environment
-const { TESTDB_USER, TESTDB_PASSWORD, TESTDB, TESTDB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_NAME, DB_HOST, TESTDB_USER, TESTDB_PASSWORD, TESTDB_NAME, TESTDB_HOST } = process.env;
 
 module.exports = {
   development: {
     username: DB_USER,
     password: DB_PASSWORD,
-    database: DB,
+    database: DB_NAME,
     DB_host: DB_HOST,
     dialect: 'postgres',
   },
@@ -16,7 +14,7 @@ module.exports = {
   test: {
     username: TESTDB_USER,
     password: TESTDB_PASSWORD,
-    database: TESTDB,
+    database: TESTDB_NAME,
     DB_host: TESTDB_HOST,
     dialect: 'postgres',
     logging: false,
@@ -25,7 +23,7 @@ module.exports = {
   production: {
     username: DB_USER,
     password: DB_PASSWORD,
-    database: DB,
+    database: DB_NAME,
     DB_host: DB_HOST,
     dialect: 'postgres',
   },
--- src/assests/constants.js
+++ src/assests/constants.js
@@ -2,11 +2,11 @@ module.exports = {
   // HTTP status as constants to improve readability of code
   SUCCESS: 200,
   CREATED: 201,
-  BAD_REQUEST: 400,
-  UNAUTHORISED: 401,
-  FORBIDDEN: 403,
-  NOT_FOUND: 404,
-  ERROR: 500,
+  BAD_REQUEST_ERROR: 400,
+  UNAUTHORISED_ERROR: 401,
+  FORBIDDEN_ERROR: 403,
+  NOT_FOUND_ERROR: 404,
+  SERVER_ERROR: 500,
 
   // constants ENUM values for user verification status
   VERIFIED: 'verified',
