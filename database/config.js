60914f9dbe694262bb90c853cd39f5d6883c37fe
Olaitan Adedokun <kenney9224@gmail.com>
2022-11-29 15:22:25 +0100

    To sync the database and the table

--- database/config.js
+++ database/config.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 const { Sequelize } = require('sequelize');
 const { development } = require('./database');
+const dbTable = require('../src/models')
 
 // new database connection using sequelize client
 const sequelize = new Sequelize(development);
@@ -19,6 +20,18 @@ const databaseConnection = async () => {
 };
 
 databaseConnection();
+
+// Sync all models with the Database
+const syncDB = (db) => {
+  // force: do not drop the table if it already exist but add to it
+  db.sequelize.sync({force: false}).then(() => {
+    logger.info('Database & tables(s) synced successfully');
+  }).catch(err => {
+    logger.error('Unable to sync with the Database', err);
+  })
+}
+
+syncDB(dbTable);
 
 // export the instance of the connection
 module.exports = sequelize;
