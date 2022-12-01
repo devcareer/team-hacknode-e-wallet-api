31e42483a624a7fad2bfaab30e36b58344eded60
Olaitan Adedokun <kenney9224@gmail.com>
2022-12-01 16:13:57 +0100

    adding the created table to the DB

--- src/models/index.js
+++ src/models/index.js
@@ -33,4 +33,7 @@ Object.keys(db).forEach((modelName) => {
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
+// Add Table(s) to the Database
+db.users = require('./user.model')(sequelize, Sequelize.DataTypes);
+
 module.exports = db;
