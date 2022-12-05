a29d84d5b1db2101ef1b7696fb144d34a8187e87
Endowmissy <adeolaayo31@yahoo.com>
2022-12-05 22:45:02 +0100

    modify jwt utils file

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -9,22 +9,22 @@ const secretKey = process.env.JWT_KEY;
  */
 export default class jwtHelper {
   /**
-   * @param {object} id - The user id  to be signed
+   * @param {object} userId - The user id  to be signed
    * @param {string} secret - The JWT secret key
    * @returns {string} The JWT signed token
    */
-  static async generateacessToken(userid, secret = secretKey) {
-    const token = jwt.sign(userid, secret, { expiresIn: '1h' });
+  static async generateacessToken(userId, secret = secretKey) {
+    const token = jwt.sign(userId, secret, { expiresIn: '1h' });
     return token;
   }
 
   /**
-   * @param {object} id - The user id to be signed
+   * @param {object} userId - The user id to be signed
    * @param {string} secret - The JWT secret key
    * @returns {string} The JWT signed token
    */
-  static async refreshToken(userid, secret = secretKey) {
-    const token = jwt.sign(userid, secret, { expiresIn: '1d' });
+  static async refreshToken(userId, secret = secretKey) {
+    const token = jwt.sign(userId, secret, { expiresIn: '1d' });
     return token;
   }
 }
