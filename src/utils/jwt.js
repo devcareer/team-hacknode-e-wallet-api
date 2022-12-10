33983c0340228b51eaf98e172358f3b06ec522f4
Bright Peter <brightbassey2001@gmail.com>
2022-12-10 23:57:33 +0100

    CU-3xgy49d - Jwt and Admin authorization

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -27,4 +27,18 @@ export default class jwtHelper {
     const token = jwt.sign(userId, secret, { expiresIn: '1d' });
     return token;
   }
+
+  // authorization for logged in user
+  static async verifyUser(token, secret = secretKey) {
+    const user = jwt.verify(token, secret);
+    return user;
+  }
+
+  // authorization for admin
+  static async verifyAdmin(user) {
+    if (user.role === 'admin') {
+      return true;
+    }
+    return false;
+  }
 }
