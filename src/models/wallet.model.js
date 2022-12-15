870faea3560308883b5ac2a12e2dd024bf7c530c
kelue <austincmoon5@gmail.com>
2022-12-15 15:54:38 +0100

    modify wallet status & migration filename

--- src/models/wallet.model.js
+++ src/models/wallet.model.js
@@ -1,33 +1,30 @@
-const {VERIFIED, UNVERIFIED} = require(`../../src/utils/constants.js`);
+const { ACTIVE, INACTIVE } = require(`../../src/utils/constants.js`);
 
 module.exports = (sequelize, DataTypes) => {
-    const Wallet = sequelize.define(
-      'Wallet',
-      {
-        id: {
-          type: DataTypes.UUID,
-          defaultValue: DataTypes.UUIDV4,
-          primaryKey: true,
-        },
-        user_id: {
-          type: DataTypes.UUID,
-          allowNull: false,
-          unique: true,
-            references: {
-              model: 'users',
-              key: 'id',
-            }
-        },
-        status: {
-          type: DataTypes.ENUM(VERIFIED, UNVERIFIED),
-          allowNull: false,
-        },
-        balance: {
-          type: DataTypes.DECIMAL(10,2).UNSIGNED,
-          allowNull: false,
-        },
-      },    
-    );
-  
-    return Wallet;
-  };
+  const Wallet = sequelize.define('Wallet', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    user_id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      unique: true,
+      references: {
+        model: 'users',
+        key: 'id',
+      },
+    },
+    status: {
+      type: DataTypes.ENUM(ACTIVE, INACTIVE),
+      allowNull: false,
+    },
+    balance: {
+      type: DataTypes.DECIMAL(10, 2).UNSIGNED,
+      allowNull: false,
+    },
+  });
+
+  return Wallet;
+};
